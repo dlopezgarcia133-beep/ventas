430a9591547a3b5661a64de6935f97aed3f6ad6a
Daniel Lopez Garcia <utm21040159@utma.edu.mx>
2025-08-12 11:32:48 -0600

    cambio en la ruta revertir

--- src/pages/ChipsAdmin.tsx
+++ src/pages/ChipsAdmin.tsx
@@ -153,7 +153,7 @@ const validarChip = async (id: number, tipo_chip: string, comision?: number) => 
         setChips((prev) =>
           prev.map((c) =>
             c.id === chip.id
-              ? { ...c, comision: parseFloat(e.target.value) }
+              ? { ...c, comision_manual: parseFloat(e.target.value) }
               : c
           )
         )
